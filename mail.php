ab8ed9a4eb65adb4b7a98553683edd419223f3f1
dhanush <dhanush.dhoni007@gmail.com>
2025-04-15 13:15:48 +0530

    change

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php
+$name = $_POST['name'];
+$email= $_POST['email'];
+$subject = $_POST['subject'];
+$message= $_POST['message'];
+
+$to ="sales.imperialtensile@gmail.com";
+$subject = "New contact from $name";
+$txt = "Name = ". $name ."\r\nEmail = " . $email.  "\r\nsubject = " . $subject .  "\r\nMessage =" . $message ;
+$headers = "From: $name <$email>" . "\r\n" .
+"CC:sales.imperialtensile@gmail.com ";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
+
+
