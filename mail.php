7d91d54891cac55e09ed408ac40a09f3a4680ea1
dhanush <dhanush.dhoni007@gmail.com>
2025-04-16 15:23:31 +0530

    change

--- mail.php
+++ mail.php
@@ -4,11 +4,11 @@ $email= $_POST['email'];
 $subject = $_POST['subject'];
 $message= $_POST['message'];
 
-$to ="sales.imperialtensile@gmail.com";
+$to ="dhanush.dhoni007@gmail.com";
 $subject = "New contact from $name";
 $txt = "Name = ". $name ."\r\nEmail = " . $email.  "\r\nsubject = " . $subject .  "\r\nMessage =" . $message ;
 $headers = "From: $name <$email>" . "\r\n" .
-"CC:sales.imperialtensile@gmail.com ";
+"CC:dhanush.dhoni007@gmail.com ";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
